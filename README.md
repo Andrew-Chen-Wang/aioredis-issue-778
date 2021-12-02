033ea9dfa17c07da27f65d47c6739b4ec2564ec3
Andrew Chen Wang <acwangpython@gmail.com>
2021-12-02 22:57:52 +0000

    Refactor tests * Remove gitpod dockerfile

--- README.md
+++ README.md
@@ -5,6 +5,16 @@ in redis-py 4.0 and aioredis 2.0. This repository is
 attempting to track down where this bug came from in 
 redis-py and create a solution.
 
+Run:
+1. `docker run --rm -d -p 6379:6379 --name redis redis`
+2. `python main.py`
+3. Enter Redis CLI: `docker exec -it CONTAINER_ID redis-cli`
+4. Get your current client: `CLIENT LIST`. It's going to be the client with
+an `addr` does not start with `127.0.0.1`.
+5. Kill connection to Python: `CLIENT KILL ADDR ip:port`
+
+TODO: I can't seem to reproduce this error.
+
 ### License
 
 Licensed under Apache 2.0
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ class SRedis(redis.Redis):
     ...
 
 def main_sync():
-    redis = SRedis()
-    redis.set("sync", "something")
-    while Trye:
-        redis.get("sync")
+    r = SRedis()
+    assert r.set("sync", "something")
+    while True:
+        print(r.get("sync") == b'something')
         time.sleep(5)
 
 
@@ -17,15 +17,13 @@ class ARedis(aioredis.Redis):
     ...
 
 async def main_async():
-    redis = ARedis()
-    await redis.set("async", "something")
-
+    r = ARedis()
+    assert await r.set("async", "something")
     while True:
-        await redis.get("async")
+        print((await r.get("async")) == b"something")
         await asyncio.sleep(5)
 
 
 if __name__ == "__main__":
     main_sync()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main_async())
+    # asyncio.get_event_loop().run_until_complete(main_async())
